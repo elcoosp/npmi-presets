5de8b3f9d73bf9b216524ddc1eb2ccf32083d5bb
Louis Cospain <elcoosp@gmail.com>
2018-09-11 23:28:01 +0200

    feat: 🎸 Make prompts for preset name, asking to add more pkg
    And the autocomplete prompt on npm pakages

---
+++ src/index.js
@@ -0,0 +1,40 @@
+#!/usr/bin/env node
+const inquirer = require('inquirer')
+const throttle = require('lodash.throttle')
+const Npm = require('./Npm')
+inquirer.registerPrompt('autocomplete', require('inquirer-autocomplete-prompt'))
+
+const P = {
+	presetName: {
+		name: 'presetName',
+		message: 'Preset name :'
+	},
+	shouldAddMorePkgs: (type = '') => ({
+		name: 'shouldAddMorePkgs',
+		type: 'confirm',
+		message: `Add another ${type} package ?`
+	}),
+	selectedPkg: (type = '') => ({
+		type: 'autocomplete',
+		name: 'selectedPkg',
+		message: `Type a ${type} package then select it to add to the preset`,
+		source: throttle(
+			async (answersSoFar, input) => Npm.search(input || '', 6),
+			1000
+		)
+	})
+}
+
+const getSelectedPackages = async (type, askAgain = true, pkgs = []) => {
+	if (!askAgain) return pkgs
+	const { selectedPkg, shouldAddMorePkgs } = await inquirer.prompt([
+		P.selectedPkg(type),
+		P.shouldAddMorePkgs(type)
+	])
+	return getSelectedPackages(type, shouldAddMorePkgs, pkgs.concat(selectedPkg))
+}
+;(async () => {
+	const deps = await getSelectedPackages('REGULAR DEPENDENCY')
+	const devDeps = await getSelectedPackages('DEV DEPENDENCY')
+	console.log({ deps, devDeps })
+})()
